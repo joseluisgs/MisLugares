2fa7264a2ef8f561504a0bcedc43ed4624158f5e
joseluisgs <joseluisgonsan@gmail.com>
2020-11-26 17:12:49 +0100

    Lugares Modelo y Controllador

---
+++ app/src/main/java/com/joseluisgs/mislugares/Lugares/Lugar.kt
@@ -0,0 +1,50 @@
+package com.joseluisgs.mislugares.Lugares
+
+import com.joseluisgs.mislugares.Usuarios.Usuario
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+import io.realm.annotations.Required
+
+/**
+ * Clase que modela el lugar
+ * @property id Long
+ * @property nombre String
+ * @property tipo String
+ * @property fecha String
+ * @property latitud String
+ * @property longitud String
+ * @property imagen String
+ * @property favorito Boolean
+ * @property votos Int
+ * @property usuarioID: Long
+ * @constructor
+ */
+@RealmClass
+open class Lugar(
+    @PrimaryKey
+    var id: Long = 0,
+    @Required
+    var nombre: String = "",
+    @Required
+    var tipo: String = "Ciudad",
+    @Required
+    var fecha: String = "",
+    @Required
+    var latitud: String = "",
+    @Required
+    var longitud: String = "",
+    var imagen: String = "",
+    var favorito: Boolean = false,
+    var votos: Int = 0,
+    var usuarioID: Long = 0
+): RealmObject() {
+    constructor(nombre: String, tipo: String, fecha: String, latitud: String, longitud: String, imagen: String, favorito: Boolean, votos: Int, usuarioID: Long) :
+            this((System.currentTimeMillis() / 1000L), nombre, tipo, fecha, latitud, longitud, imagen, favorito, votos, usuarioID)
+
+    override fun toString(): String {
+        return "Lugar(id=$id, nombre='$nombre', tipo='$tipo', fecha='$fecha', latitud='$latitud', longitud='$longitud', imagen='$imagen', favorito=$favorito, votos=$votos, usuarioID=$usuarioID)"
+    }
+
+
+}
---
+++ app/src/main/java/com/joseluisgs/mislugares/Lugares/LugarController.kt
@@ -0,0 +1,66 @@
+package com.joseluisgs.mislugares.Lugares
+
+import io.realm.Realm
+import io.realm.kotlin.where
+
+object LugarController  {
+    /**
+     * Inserta un lugar
+     * @param lugar Lugar
+     */
+    fun insert(lugar: Lugar) {
+        Realm.getDefaultInstance().executeTransaction {
+            it.copyToRealm(lugar); // Copia, inserta
+        }
+    }
+
+    /**
+     * Borra un lugar del sistema
+     * @param lugar Lugar
+     */
+    fun delete(lugar: Lugar) {
+        Realm.getDefaultInstance().executeTransaction {
+            it.where<Lugar>().equalTo("id", lugar.id).findFirst()?.deleteFromRealm()
+        }
+    }
+
+    /**
+     * Actualiza un lugar en el sistema de almacenamiento
+     */
+    fun update(lugar: Lugar) {
+        Realm.getDefaultInstance().executeTransaction {
+            it.copyToRealmOrUpdate(lugar)
+        }
+    }
+
+    /**
+     * Busca lugar por nombre
+     * @param login String
+     * @return Lugar?
+     */
+    fun selectByNombre(nombre: String): Lugar? {
+        return Realm.getDefaultInstance().copyFromRealm(
+            Realm.getDefaultInstance().where<Lugar>().equalTo("nombre", nombre).findFirst()
+        )
+    }
+
+    /**
+     * Busca lugares por ID
+     * @param id Long
+     * @return Usuario?
+     */
+    fun selectById(id: Long): Lugar? {
+        return Realm.getDefaultInstance().copyFromRealm(
+            Realm.getDefaultInstance().where<Lugar>().equalTo("id", id).findFirst()
+        )
+    }
+
+    /**
+     * Elimina todos los objetos
+     */
+    fun removeAll() {
+        Realm.getDefaultInstance().executeTransaction {
+            Realm.getDefaultInstance().where<Lugar>().findAll().deleteAllFromRealm();
+        }
+    }
+}
