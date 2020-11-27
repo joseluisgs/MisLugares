af5d62efabbf82910f7e3466e379867ee354a35d
joseluisgs <joseluisgonsan@gmail.com>
2020-11-27 18:15:00 +0100

    Interfaces Lugar y Lugar Detalle

---
+++ app/src/main/java/com/joseluisgs/mislugares/UI/lugares/LugaresFragment.kt
@@ -0,0 +1,25 @@
+package com.joseluisgs.mislugares.UI.lugares
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.joseluisgs.mislugares.R
+
+class LugaresFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_lugares, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i("Lugares", "Creando Lista Lugares")
+
+    }
+}
