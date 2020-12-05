b56d7421187c0407af5028c92f0ed5e51d3b16b8
joseluisgs <joseluisgonsan@gmail.com>
2020-12-05 16:49:30 +0100

    Interfaz inicial

--- app/src/main/java/com/joseluisgs/mislugares/UI/mapa/MapaFragment.kt
+++ app/src/main/java/com/joseluisgs/mislugares/UI/mapa/MapaFragment.kt
@@ -6,9 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.UiSettings
+import com.google.android.gms.maps.model.LatLng
 import com.joseluisgs.mislugares.R
 
-class MapaFragment : Fragment() {
+class MapaFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+    private var posicion: LatLng? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +28,48 @@ class MapaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.i("Mapa", "Creando Mapa")
+        view.setOnTouchListener { view, motionEvent ->
+            return@setOnTouchListener true
+        }
+        initUI()
 
+    }
+
+    private fun initUI() {
+        initMapa()
+    }
+
+    /**
+     * Inicia el Mapa
+     */
+    private fun initMapa() {
+        Log.i("Mapa", "Iniciando Mapa")
+        val mapFragment = (childFragmentManager
+            .findFragmentById(R.id.miMapa) as SupportMapFragment?)!!
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * EL mapa está listo
+     * @param googleMap GoogleMap
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        configurarIUMapa()
+    }
+
+    /**
+     * Configuración por defecto del modo de mapa
+     */
+    private fun configurarIUMapa() {
+        Log.i("Mapa", "Configurando IU Mapa")
+        mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+        val uiSettings: UiSettings = mMap.uiSettings
+        uiSettings.isScrollGesturesEnabled = true
+        uiSettings.isTiltGesturesEnabled = true
+        uiSettings.isCompassEnabled = true
+        uiSettings.isZoomControlsEnabled = true
+        uiSettings.isMapToolbarEnabled = true
+        // mMap.setMinZoomPreference(12.0f)
     }
 }
