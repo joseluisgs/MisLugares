bd2dfb5bd6a9d5a757b6c817b60747a26fd047ec
joseluisgs <joseluisgonsan@gmail.com>
2020-12-11 15:15:28 +0100

    Sesiones DTO y Mapper

--- app/src/main/java/com/joseluisgs/mislugares/Entidades/Sesiones/SesionDTO.kt
+++ app/src/main/java/com/joseluisgs/mislugares/Entidades/Sesiones/SesionDTO.kt
@@ -1,4 +1,16 @@
 package com.joseluisgs.mislugares.Entidades.Sesiones
 
-class SesionDTO {
-}
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Traformador de Sesiones
+ * @property id String
+ * @property time String
+ * @property token String
+ * @constructor
+ */
+class SesionDTO(
+    @SerializedName("id") val id: String,
+    @SerializedName("time") val time: String,
+    @SerializedName("token") val token: String,
+)
--- app/src/main/java/com/joseluisgs/mislugares/Entidades/Sesiones/SesionMapper.kt
+++ app/src/main/java/com/joseluisgs/mislugares/Entidades/Sesiones/SesionMapper.kt
@@ -1,4 +1,50 @@
 package com.joseluisgs.mislugares.Entidades.Sesiones
 
-class SesionMapper {
+/**
+ * Mapea entre DTO y Clase Modelo
+ */
+object SesionMapper {
+    /**
+     * Una lista de DTO a Modelos
+     * @param items List<SesionDTO>
+     * @return List<Sesion>
+     */
+    fun fromDTO(items: List<SesionDTO>): List<Sesion> {
+        return items.map { fromDTO(it) }
+    }
+
+    /**
+     * Una lista de Modelos a DTO
+     * @param items List<Sesion>
+     * @return List<SesionDTO>
+     */
+    fun toDTO(items: List<Sesion>): List<SesionDTO> {
+        return items.map { toDTO(it) }
+    }
+
+    /**
+     * DTO a Modelo
+     * @param dto SesionDTO
+     * @return Sesion
+     */
+    fun fromDTO(dto: SesionDTO): Sesion {
+        return Sesion(
+            dto.id,
+            dto.time,
+            dto.token
+        )
+    }
+
+    /**
+     * Modelo a DTO
+     * @param model Sesion
+     * @return SesionDTO
+     */
+    fun toDTO(model: Sesion): SesionDTO {
+        return SesionDTO(
+            model.usuarioID,
+            model.time,
+            model.token
+        )
+    }
 }
