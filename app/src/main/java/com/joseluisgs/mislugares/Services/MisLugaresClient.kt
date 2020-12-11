79ce639c9f8fc320ed563c13c012404410526349
joseluisgs <joseluisgonsan@gmail.com>
2020-12-11 15:35:35 +0100

    Mis Lugares API Retrofit

---
+++ app/src/main/java/com/joseluisgs/mislugares/Services/MisLugaresClient.kt
@@ -0,0 +1,16 @@
+package com.joseluisgs.mislugares.Services
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object MisLugaresClient {
+    private var retrofit: Retrofit? = null
+    fun getClient(url: String): Retrofit? {
+        if (retrofit == null) {
+            retrofit = Retrofit.Builder().baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return retrofit
+    }
+}
---
+++ app/src/main/java/com/joseluisgs/mislugares/Services/MisLugaresREST.kt
@@ -0,0 +1,30 @@
+package com.joseluisgs.mislugares.Services
+
+import retrofit2.http.GET
+
+interface MisLugaresREST {
+//    // Obtener todos
+//    // https://my-json-server.typicode.com/joseluisgs/APIRESTFake/users
+//    @GET("users/")
+//    fun findAll(): Call<List<UsuarioDTO>>
+//
+//    // Obtener por ID
+//    // GET: https://my-json-server.typicode.com/joseluisgs/APIRESTFake/users/{id}
+//    @GET("users/{id}")
+//    fun findById(@Path("id") id: String): Call<UsuarioDTO>
+//
+//    // Crear un item
+//    //POST: https://my-json-server.typicode.com/joseluisgs/APIRESTFake/users/
+//    @POST("users/")
+//    fun create(@Body user: UsuarioDTO): Call<UsuarioDTO>
+//
+//    // Elimina un item
+//    // DELETE: https://my-json-server.typicode.com/joseluisgs/APIRESTFake/users/{id}
+//    @DELETE("users/{id}")
+//    fun delete(@Path("id") id: String): Call<UsuarioDTO>
+//
+//    // Actualiza un producto
+//    // PUT: https://my-json-server.typicode.com/joseluisgs/APIRESTFake/users/{id}
+//    @PUT("users/{id}")
+//    fun update(@Path("id") id: String, @Body producto: UsuarioDTO): Call<UsuarioDTO>
+}
