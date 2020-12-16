3060f080357c2d9f71694db5f0570f9f88e00b74
joseluisgs <joseluisgonsan@gmail.com>
2020-12-16 22:34:30 +0100

    Obtención de posición evento

--- app/src/main/java/com/joseluisgs/mislugares/UI/tiempo/TiempoFragment.kt
+++ app/src/main/java/com/joseluisgs/mislugares/UI/tiempo/TiempoFragment.kt
@@ -5,16 +5,23 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import com.joseluisgs.mislugares.R
 import kotlinx.android.synthetic.main.fragment_brujula.*
 
 class TiempoFragment: Fragment() {
-
+    private lateinit var mPosicion: FusedLocationProviderClient
+    private lateinit var localizacion: Location
+    private lateinit var posicion: LatLng
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +33,13 @@ class TiempoFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mPosicion = LocationServices.getFusedLocationProviderClient(activity!!)
+        // Evento que obtiene la posición
+        mPosicion.lastLocation
+            .addOnSuccessListener { location : Location? ->
+               this.localizacion = location!!
+                Log.i("Tiempo", this.localizacion.toString())
+            }
     }
 
 }
