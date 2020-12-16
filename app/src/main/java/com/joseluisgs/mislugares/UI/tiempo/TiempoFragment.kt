2054a70e5d7029b2a79cd8eb06a8d02b7f9114ef
joseluisgs <joseluisgonsan@gmail.com>
2020-12-16 23:09:45 +0100

    Api Temperatura

--- app/src/main/java/com/joseluisgs/mislugares/UI/tiempo/TiempoFragment.kt
+++ app/src/main/java/com/joseluisgs/mislugares/UI/tiempo/TiempoFragment.kt
@@ -15,8 +15,15 @@ import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
+import com.joseluisgs.mislugares.Entidades.Tiempo.WeatherResponse
 import com.joseluisgs.mislugares.R
+import com.joseluisgs.mislugares.Services.MisLugaresAPI
+import com.joseluisgs.mislugares.Services.Tiempo.ElTiempoAPI
 import kotlinx.android.synthetic.main.fragment_brujula.*
+import kotlinx.android.synthetic.main.fragment_tiempo.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TiempoFragment: Fragment() {
     private lateinit var mPosicion: FusedLocationProviderClient
@@ -39,7 +46,49 @@ class TiempoFragment: Fragment() {
             .addOnSuccessListener { location : Location? ->
                this.localizacion = location!!
                 Log.i("Tiempo", this.localizacion.toString())
+                obtenerInformacionTiempo()
             }
+    }
+
+    /**
+     * Obtiene información del Tiempo
+     */
+    private fun obtenerInformacionTiempo() {
+        val clientREST = ElTiempoAPI.service
+        val call = clientREST.getCurrentWeatherData(this.localizacion.latitude.toString(), this.localizacion.longitude.toString(),
+            ElTiempoAPI.API_KEY, ElTiempoAPI.UNITS, ElTiempoAPI.LANG)
+
+        call.enqueue(object : Callback<WeatherResponse> {
+            override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+                if (response.code() == 200) {
+                    val weatherResponse = response.body()!!
+
+                    val stringBuilder = "Country: " +
+                            weatherResponse.sys!!.country +
+                            "\n" +
+                            "Temperature: " +
+                            weatherResponse.main!!.temp +
+                            "\n" +
+                            "Temperature(Min): " +
+                            weatherResponse.main!!.temp_min +
+                            "\n" +
+                            "Temperature(Max): " +
+                            weatherResponse.main!!.temp_max +
+                            "\n" +
+                            "Humidity: " +
+                            weatherResponse.main!!.humidity +
+                            "\n" +
+                            "Pressure: " +
+                            weatherResponse.main!!.pressure
+
+                    tiempoText!!.text = stringBuilder
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                tiempoText!!.text = t.message
+            }
+        })
     }
 
 }
