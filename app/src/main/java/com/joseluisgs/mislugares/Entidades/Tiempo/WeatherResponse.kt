89376f3634b1532ff702406b3ee222f46c7d6afe
joseluisgs <joseluisgonsan@gmail.com>
2020-12-16 22:38:25 +0100

    Modelo Tiempo

---
+++ app/src/main/java/com/joseluisgs/mislugares/Entidades/Tiempo/WeatherResponse.kt
@@ -0,0 +1,29 @@
+package com.joseluisgs.mislugares.Entidades.Tiempo
+
+import com.google.gson.annotations.SerializedName
+
+class WeatherResponse {
+    @SerializedName("coord")
+    var coord: Coord? = null
+    @SerializedName("sys")
+    var sys: Sys? = null
+    @SerializedName("weather")
+    var weather = ArrayList<Weather>()
+    @SerializedName("main")
+    var main: Main? = null
+    @SerializedName("wind")
+    var wind: Wind? = null
+    @SerializedName("rain")
+    var rain: Rain? = null
+    @SerializedName("clouds")
+    var clouds: Clouds? = null
+    @SerializedName("dt")
+    var dt: Float = 0.toFloat()
+    @SerializedName("id")
+    var id: Int = 0
+    @SerializedName("name")
+    var name: String? = null
+    @SerializedName("cod")
+    var cod: Float = 0.toFloat()
+}
+
