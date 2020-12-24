b684e2f5cafd5017e9d9f34662462e7bc81d72d8
joseluisgs <joseluisgonsan@gmail.com>
2020-12-24 15:03:36 +0100

    Messaging init

---
+++ app/src/main/java/com/joseluisgs/mislugares/Services/Firebase/FirebaseMessagingService.kt
@@ -0,0 +1,7 @@
+package com.joseluisgs.mislugares.Services.Firebase
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService:  FirebaseMessagingService() {
+
+}
